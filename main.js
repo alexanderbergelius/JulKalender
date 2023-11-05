06b9f09636e8d8360929501192e04a1f206a8e3b
alexanderbergelius <alexander.bergelius@student.tabyenskilda.se>
2023-11-05 19:30:01 +0100

    Alla går att flippa

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-const card = document.querySelector(".card__inner");
+const cards = document.querySelectorAll(".card__inner");
 
-card.addEventListener("click", function (e) {
-  card.classList.toggle('is-flipped');
+cards.forEach(card => {
+  card.addEventListener("click", function (e) {
+    card.classList.toggle('is-flipped');
+  });
 });
-
-mytoggle
-
-
-
-
